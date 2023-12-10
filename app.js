7fb402d0e62404cad375caecc16488c59e697882
DcM12345 <d.cuesta.2022@alumnos.urjc.es>
2023-12-10 18:16:10 +0100

    Prueba2

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const { publicDecrypt } = require("crypto");
 const express = require("express")
 const app= express();
-const Port= 3000
-app.use(express.static('public'));
+const Port= 3456
+app.use(express.static('styles'));
+app.use(express.static('public'))
 app.get('/', (req,res)=> {
     res.sendFile(__dirname+ '/public/Practica1.html')
-
-
 })
 app.listen(Port,()=>{
-    console.log('Servidor escuchando en puerto', 3000)
+    console.log('Servidor escuchando en puerto', Port)
 })
