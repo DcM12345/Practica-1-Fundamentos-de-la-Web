871abcb81071d74b82ad74db232275dc7d5c113f
DcM12345 <d.cuesta.2022@alumnos.urjc.es>
2023-12-11 17:12:15 +0100

    Elementos guardados

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const { publicDecrypt } = require("crypto");
 const express = require("express")
+const fs = require("fs")
 const app= express();
-const Port= 3567
+const Port= 3001
 app.use(express.static('styles'));
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+var notesHTML = ""
+var files = fs.readdirSync(__dirname + '/tmp');
+for (var filename of files) {
+    notesHTML += fs.readFileSync(__dirname + '/tmp/' + filename)
+}
+
 app.get('/', (req,res)=> {
     res.sendFile(__dirname+ '/public/Practica1.html')
+})
+// app.get('/note1', (req,res)=> {
+//     nota1 = fs.readFileSync(dirname+ '/tmp/nota1.html')
+//     res.send(nota1.toString())
+// })
+app.get('/notesList', (req,res)=> {
+    res.send(notesHTML)
 })
 app.listen(Port,()=>{
     console.log('Servidor escuchando en puerto', Port)
