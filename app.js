66fc0acfcec7402c08f82411d8d3a97ce4ee07a6
DcM12345 <d.cuesta.2022@alumnos.urjc.es>
2023-12-11 19:36:09 +0100

    Añadir nota y Pruebas Pagina detalle

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
-const { publicDecrypt } = require("crypto");
-const express = require("express")
-const fs = require("fs")
-const app= express();
-const Port= 3001
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const Port = 3024;
+
 app.use(express.static('styles'));
 app.use(express.static('public'));
+app.use(express.static('tmp'));
+app.use(express.urlencoded({ extended: true }));
 
-var notesHTML = ""
-var files = fs.readdirSync(__dirname + '/tmp');
-for (var filename of files) {
-    notesHTML += fs.readFileSync(__dirname + '/tmp/' + filename)
-}
 
-app.get('/', (req,res)=> {
-    res.sendFile(__dirname+ '/public/Practica1.html')
-})
-// app.get('/note1', (req,res)=> {
-//     nota1 = fs.readFileSync(dirname+ '/tmp/nota1.html')
-//     res.send(nota1.toString())
-// })
-app.get('/notesList', (req,res)=> {
-    res.send(notesHTML)
-})
-app.listen(Port,()=>{
-    console.log('Servidor escuchando en puerto', Port)
-})
+app.get('/error', (req, res) => {
+    res.sendFile(__dirname + '/public/error.html');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/Practica1.html');
+});
+
+app.get('/notesList', (req, res) => {
+    var notesHTML = "";
+    var files = fs.readdirSync(__dirname + '/tmp');
+    for (var filename of files) {
+        notesHTML += fs.readFileSync(__dirname + '/tmp/' + filename);
+    }
+    res.send(notesHTML);
+});
+
+app.post('/guardarNota', (req, res) => {
+    const { subelemento, descripcion, imagen } = req.body;
+
+   
+    if (!subelemento || !descripcion || !imagen) {
+        
+        res.redirect('/error');
+        return;
+    }
+
+    const files = fs.readdirSync(__dirname + '/tmp');
+
+   
+    const nextNumber = files.length + 1;
+
+    const fileName = `Nota${nextNumber}.html`;
+
+
+    const notaContent = `<div class="col-md-4">
+        <a href="PaginaDetalle.html?nota=${nextNumber}" class="tarjeta">
+            <div class="card mb-4">
+                <img src="${imagen}" class="card-img-top" alt="${subelemento}">
+                <div class="card-body">
+                    <h5>${subelemento}</h5>
+                </div>
+            </div>
+        </a>
+    </div>`;
+
+    fs.writeFileSync(__dirname + '/tmp/' + fileName, notaContent);
+
+   
+    res.redirect('/');
+});
+
+app.listen(Port, () => {
+    console.log('Servidor escuchando en puerto', Port);
+});
